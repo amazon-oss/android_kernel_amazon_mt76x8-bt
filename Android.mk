bd0ada1c07fcd58ee34c6f84b1c45bf3bf4fbe34
bengris32 <bengris32@protonmail.ch>
2025-10-11 20:13:33 +0100

    mt76x8-bt: Add Android build support
    Change-Id: I237e1ddeb5c6c4c79babced89b18a1c36e8104ac
    Signed-off-by: bengris32 <bengris32@protonmail.ch>

---
+++ Android.mk
@@ -0,0 +1,32 @@
+LOCAL_PATH := $(call my-dir)
+
+ifeq ($(TARGET_PREBUILT_KERNEL),)
+MT76X8_BT_PATH := kernel/amazon/mt76x8-bt
+
+include $(CLEAR_VARS)
+
+LOCAL_MODULE        := mt76x8_bt
+LOCAL_MODULE_SUFFIX := .ko
+LOCAL_MODULE_CLASS  := ETC
+LOCAL_MODULE_PATH   := $(TARGET_OUT_VENDOR)/lib/modules
+
+_mt76x8_bt_intermediates := $(call intermediates-dir-for,$(LOCAL_MODULE_CLASS),$(LOCAL_MODULE))
+_mt76x8_bt_mod_name := $(LOCAL_MODULE)
+_mt76x8_bt_ko := $(_mt76x8_bt_intermediates)/$(_mt76x8_bt_mod_name)$(LOCAL_MODULE_SUFFIX)
+KERNEL_OUT := $(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ
+KERNEL_OUT_RELATIVE := ../../KERNEL_OBJ
+
+$(_mt76x8_bt_ko): $(KERNEL_OUT)/arch/$(KERNEL_ARCH)/boot/$(BOARD_KERNEL_IMAGE_NAME)
+	@mkdir -p $(dir $@)
+	@mkdir -p $(KERNEL_MODULES_OUT)/lib/modules
+	@cp -R $(MT76X8_BT_PATH)/module/* $(_mt76x8_bt_intermediates)/
+	$(hide) +$(KERNEL_MAKE_CMD) $(KERNEL_MAKE_FLAGS) -C $(KERNEL_OUT) M=$(abspath $(_mt76x8_bt_intermediates)) ARCH=$(KERNEL_ARCH) $(KERNEL_CROSS_COMPILE) SDIO_MOD_NAME=$(_mt76x8_bt_mod_name) modules
+	modules=$$(find $(_mt76x8_bt_intermediates) -type f -name '*.ko'); \
+	for f in $$modules; do \
+		$(KERNEL_TOOLCHAIN_PATH)strip --strip-unneeded $$f; \
+		cp $$f $(KERNEL_MODULES_OUT)/lib/modules; \
+	done;
+	touch $(_mt76x8_bt_ko)
+
+include $(BUILD_SYSTEM)/base_rules.mk
+endif
